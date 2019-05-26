ce17b09972239f65c0018a28a868c3d68c8fd9b1
gina chen <ginachen588@gmail.com>
2019-05-26 14:15:40 +0100

    added layouts and home page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,21 @@
-import "bootstrap";
+
+
+
+var results = document.getElementById("results");
+
+fetch("https://www.mangaeden.com/api/list/0/")
+  .then(response => response.json())
+  .then((data) => {
+
+    data.forEach((result) => {
+
+        const movies = '<li><img src="'+result.im+'" alt=""><h3>'+result.a+'</h3><p>'+result.c+'</p></li>';
+        results.insertAdjacentHTML("beforeend", movies);
+
+    });
+  });
+
+
+
+
+
