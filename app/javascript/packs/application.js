24521699024e6e883de3fdaf1fd831b73491cb33
gina chen <ginachen588@gmail.com>
2019-05-27 16:40:43 +0100

    added js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,21 +1,15 @@
-
-
-
 var results = document.getElementById("results");
 
-fetch("https://www.mangaeden.com/api/list/0/")
+fetch("https://www.mangaeden.com/api/list/0/", {
+    mode: 'cors'
+  })
   .then(response => response.json())
   .then((data) => {
 
-    data.forEach((result) => {
+    data.manga.slice(3, 50).forEach((result) => {
 
-        const movies = '<li><img src="'+result.im+'" alt=""><h3>'+result.a+'</h3><p>'+result.c+'</p></li>';
-        results.insertAdjacentHTML("beforeend", movies);
+      const movies = '<li><img src="https://cdn.mangaeden.com/mangasimg/' + result.im + '" alt=""><h3>' + result.a + '</h3><p>' + result.c + '</p></li>';
+      results.insertAdjacentHTML("beforeend", movies);
 
     });
   });
-
-
-
-
-
