ede70f7bcc6040fc20a8e80ccefa8629b409fbf0
gina chen <ginachen588@gmail.com>
2019-05-29 12:32:45 +0100

    added js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,9 @@ fetch("https://www.mangaeden.com/api/list/0/", {
   .then(response => response.json())
   .then((data) => {
 
-    data.manga.slice(3, 50).forEach((result) => {
+    data.manga
+      .filter(movie => movie.c.includes('Action') || movie.c.includes('Adventure'))
+      .slice(0, 40).forEach((result) => {
 
       const movies = '<li><img src="https://cdn.mangaeden.com/mangasimg/' + result.im + '" alt=""><h3>' + result.a + '</h3><p>' + result.c + '</p></li>';
       results.insertAdjacentHTML("beforeend", movies);
